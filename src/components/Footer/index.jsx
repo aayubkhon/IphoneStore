a90488d59ed86ca6fd4956b0d98c47122eaa7fb5
aayubkhoncd <aayubxon701@gmail.com>
2022-07-14 11:41:36 +0900

    root

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -58,22 +58,3 @@ export default Footer
 
 
 
-{/* <Title>
-<Title.Paragraph>Company</Title.Paragraph>
-<Title.Paragraphs>About</Title.Paragraphs>
-<Title.Paragraphs>Support</Title.Paragraphs>
-</Title>
-<Title>
-<Title.Paragraph>Support</Title.Paragraph>
-<Title.Paragraphs>Style Guide</Title.Paragraphs>
-<Title.Paragraphs>Licensing</Title.Paragraphs>
-<Title.Paragraphs>Change Log</Title.Paragraphs>
-<Title.Paragraphs>Contact</Title.Paragraphs>
-</Title>
-<Title>
-<Title.Paragraph>Follow Us</Title.Paragraph>
-<Title.Paragraphs>Instagram</Title.Paragraphs>
-<Title.Paragraphs>Facebook</Title.Paragraphs>
-<Title.Paragraphs>LinkedIn</Title.Paragraphs>
-<Title.Paragraphs>Youtube</Title.Paragraphs>
-</Title> */}
